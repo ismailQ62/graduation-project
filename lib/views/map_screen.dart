af9f9423bc1c43d6d1af1c519ce280bf1a96c5ca
Jood-X <joodmansi3@gmail.com>
2025-03-15 01:58:36 +0300

    Map Screen updates
    Current location on map.
    Search feature and route to destination.

--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart' as geolocator;
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -11,78 +16,285 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  //final MapController _mapController = MapController();
-  LatLng myCurrentLocation = const LatLng(32.4951,35.9912);
-  late MapController mapController;
-  Set<Marker> marker ={};
+  final MapController _mapController = MapController();
+  final Location _location = Location();
+  final TextEditingController _locationController = TextEditingController();
+  bool isLoading = true;
+  LatLng? _currentLocation;
+  LatLng? _destination;
+  List<LatLng> _route = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeLocation();
+  }
+
+  Future<void> _initializeLocation() async {
+    geolocator.LocationPermission permission =
+        await geolocator.Geolocator.requestPermission();
+
+    if (permission == geolocator.LocationPermission.denied ||
+        permission == geolocator.LocationPermission.deniedForever) {
+      errorMessage("Location permission is denied.");
+      return;
+    }
+
+    geolocator.Position position = await geolocator
+        .Geolocator.getCurrentPosition(
+      locationSettings: geolocator.LocationSettings(
+        accuracy: geolocator.LocationAccuracy.high,
+        distanceFilter: 10, // Minimum distance before an update
+      ),
+    );
+
+    setState(() {
+      _currentLocation = LatLng(position.latitude, position.longitude);
+      isLoading = false;
+    });
+  }
+
+  // Method to fetch coordinates for a given location using the OpenStreetMap Nomination API
+  Future<void> fetchCoordinatesPoint(String location) async {
+    if (location.isEmpty) {
+      errorMessage("Please enter a location.");
+      return;
+    }
+
+    final url = Uri.parse(
+      "https://nominatim.openstreetmap.org/search?format=json&q=$location&limit=1",
+    );
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {"User-Agent": "Flutter-App"},
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+
+        if (data.isNotEmpty) {
+          final lat = double.parse(data[0]['lat']);
+          final lon = double.parse(data[0]['lon']);
+
+          setState(() {
+            _destination = LatLng(lat, lon);
+            _mapController.move(_destination!, 15); // Move map to new location
+          });
+
+          await fetchRoute(); // Fetch route after setting destination
+        } else {
+          errorMessage("Location not found. Try another search.");
+        }
+      } else {
+        errorMessage("Failed to fetch location. Try again later.");
+      }
+    } catch (e) {
+      errorMessage("Error fetching location: $e");
+    }
+  }
+
+  // Method to fetch the route between the current location and the destination using the OSRM API.
+  Future<void> fetchRoute() async {
+    if (_currentLocation == null || _destination == null) return;
+    final url = Uri.parse(
+      "http://router.project-osrm.org/route/v1/driving/"
+      '${_currentLocation!.longitude},${_currentLocation!.latitude};'
+      '${_destination!.longitude},${_destination!.latitude}?overview=full&geometries=polyline',
+    );
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final geometry = data['routes'][0]['geometry'];
+      _decodePolyline(geometry);
+    } else {
+      errorMessage("Failed to fetch route. Try again later.");
+    }
+  }
+
+  //Method to decode a polyline string into a list of geographic coordinates
+  void _decodePolyline(String encodedPolyline) {
+    PolylinePoints polylinePoints = PolylinePoints();
+    List<PointLatLng> decodedPoints = polylinePoints.decodePolyline(
+      encodedPolyline,
+    );
+    setState(() {
+      _route =
+          decodedPoints
+              .map((point) => LatLng(point.latitude, point.longitude))
+              .toList();
+    });
+  }
+
+  // Method to check permissions
+  Future<bool> _checkTheRequestPermissions() async {
+    bool serviceEnabled = await _location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await _location.requestService();
+      if (!serviceEnabled) return false;
+    }
+    PermissionStatus permissionGranted = await _location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await _location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<void> _userCurrentLocation() async {
+    if (_currentLocation != null) {
+      _mapController.move(_currentLocation!, 15);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Current Location Not Available.")),
+      );
+    }
+  }
+  // Method to display an error
+  void errorMessage(String message) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Map')), 
-      body: content());
-  }
-
-  Widget content() {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(32.55828, 35.87560),
-        initialZoom: 15,
-        minZoom: 0,
-        maxZoom: 100,
-        interactionOptions: const InteractionOptions(
-          flags: ~InteractiveFlag.doubleTapZoom,
-        ),
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        title: Text('Map'),
+        backgroundColor: Colors.grey,
       ),
-      children: [
-        openStreetMapTileLater,
-        _searchField(),
-        CurrentLocationLayer(
-          style: const LocationMarkerStyle(
-            marker: DefaultLocationMarker(
-              child: Icon(
-                Icons.location_pin,
-                color: Colors.white,
-              )
+      body: Stack(
+        children: [
+          // isLoading
+          //     ? const Center(child: CircularProgressIndicator(),
+          //     ):
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: _currentLocation ?? LatLng(32.55828, 35.87560),
+              initialZoom: 15,
+              minZoom: 0,
+              maxZoom: 100,
             ),
-            markerSize: Size(35, 35),
-            markerDirection: MarkerDirection.heading,
-          ),
-        )
-      ],
-    );
-    
-  }
-  Container _searchField() {
-    return Container(
-          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-          child: TextField(
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xffF8F9FE),
-                contentPadding: EdgeInsets.all(15),
-                hintText: 'Search',
-                hintStyle: TextStyle(
-                  color: Color(0xff8F9098),
-                  fontSize: 14,
-                ),
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: 
-                  Icon(Icons.search),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(24),
-                  borderSide: BorderSide.none,
-                )
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               ),
+              //openStreetMapTileLater,
+              //_searchField(),
+              CurrentLocationLayer(
+                style: const LocationMarkerStyle(
+                  marker: DefaultLocationMarker(
+                    child: Icon(Icons.location_pin, color: Colors.white),
+                  ),
+                  markerSize: Size(35, 35),
+                  markerDirection: MarkerDirection.heading,
+                ),
+              ),
+              if (_destination != null)
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: _destination!,
+                      width: 50,
+                      height: 50,
+                      child: Icon(
+                        Icons.location_pin,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              if (_currentLocation != null &&
+                  _destination != null &&
+                  _route.isNotEmpty)
+                PolylineLayer(
+                  polylines: [
+                    Polyline(points: _route, strokeWidth: 5, color: Colors.red),
+                  ],
+                ),
+            ],
           ),
-        );
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _locationController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Enter a location',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    style: IconButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {
+                      final location = _locationController.text.trim();
+                      if (location.isNotEmpty) {
+                        fetchCoordinatesPoint(location);
+                      }
+                    },
+                    icon: const Icon(Icons.search),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 0,
+        onPressed: _userCurrentLocation,
+        backgroundColor: Colors.grey,
+        child: const Icon(Icons.my_location, size: 30, color: Colors.white),
+      ),
+    );
   }
 }
 
+Container _searchField() {
+  return Container(
+    margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+    child: TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0xffF8F9FE),
+        contentPadding: EdgeInsets.all(15),
+        hintText: 'Search',
+        hintStyle: TextStyle(color: Color(0xff8F9098), fontSize: 14),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Icon(Icons.search),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(24),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
 TileLayer get openStreetMapTileLater => TileLayer(
   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
   userAgentPackageName: 'dev.fleatflet.flutter_map.example',
 );
-
